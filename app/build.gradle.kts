b3e41682fe632eb9985b8f3bab62f6b6fc5bed16
vagabond911 <vageeshbits@gmail.com>
2026-02-11 23:37:00 -0500

    nn

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
+// Retrofit
+    implementation("com.squareup.retrofit2:retrofit:3.0.0")
+// Retrofit with Scalar Converter
+    implementation("com.squareup.retrofit2:converter-scalars:3.0.0")
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
